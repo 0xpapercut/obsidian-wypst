0f805f0058a1df893be18442762e09860d57c218
0xpapercut <0xpapercut@gmail.com>
2024-01-23 22:44:32 +0100

    Use katex as math processor

--- main.ts
+++ main.ts
@@ -1,17 +1,8 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, MarkdownRenderer, Component, TFile, loadMathJax } from 'obsidian';
+import { Plugin, loadMathJax } from 'obsidian';
 import katex from 'katex';
 import wypst from 'wypst';
 
-interface WypstSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: WypstSettings = {
-	mySetting: 'default'
-}
-
 export default class Wypst extends Plugin {
-	settings: WypstSettings;
 	_tex2chtml;
 
 	wasmPath = this.app.vault.configDir + '/plugins/obsidian-wypst/core_bg.wasm';
@@ -32,140 +23,18 @@ export default class Wypst extends Plugin {
 		const parser = new DOMParser();
 		this._tex2chtml = global.MathJax.tex2chtml;
 
-
 		global.MathJax.tex2chtml = (e, r) => {
-			const renderedString = wypst.renderToString(e, {displayMode: r.display});
+			const renderSettings = {
+				throwOnError: false,
+				displayMode: r.display,
+			}
+
+			const renderedString = katex.renderToString(e, renderSettings);
 			return parser.parseFromString(renderedString, "text/html").body.firstChild;
 		}
-
-		// this.registerMarkdownPostProcessor((element, context) => {
-		// 	const codeblocks = element.findAll("code");
-
-		// 	for (const codeblock of codeblocks) {
-		// 		const text = codeblock.innerText.trim();
-		// 		if (text[0] === ":" && text[text.length - 1] === ":") {
-		// 			const emojiEl = codeblock.createSpan({
-		// 			text: ALL_EMOJIS[text] ?? text,
-		// 		});
-		// 		codeblock.replaceWith(emojiEl);
-		// 		}
-		// 	}
-		// });
-		// await this.loadSettings();
-
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-		// // This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: Wypst;
-
-	constructor(app: App, plugin: Wypst) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		global.MathJax.tex2chtml = this._tex2chtml;
 	}
 }
