50ec7c009a7a875a71cdef6cce9b595f34dbf3b7
0xpapercut <0xpapercut@gmail.com>
2024-01-28 23:40:34 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,40 +3,34 @@ import katex from 'katex';
 import wypst from 'wypst';
 
 export default class Wypst extends Plugin {
-	_tex2chtml;
-
+	_tex2chtml: any;
 	wasmPath = this.app.vault.configDir + '/plugins/obsidian-wypst/core_bg.wasm';
 
 	async onload() {
 		await loadMathJax();
 
 		if (!global.MathJax) {
-			console.log("MathJax failed to load.")
-		} else {
-			console.log("MathJax loaded.")
+			throw new Error("MathJax failed to load.");
 		}
 
-		const blob = new Blob ([await this.app.vault.adapter.readBinary(this.wasmPath)], { type: 'application/wasm' });
-		const url = URL.createObjectURL(blob);
-		await wypst.init(url);
+		const wasm = await this.app.vault.adapter.readBinary(this.wasmPath);
+		await wypst.init(wasm);
 
 		const parser = new DOMParser();
 		this._tex2chtml = global.MathJax.tex2chtml;
 
 		global.MathJax.tex2chtml = (e, r) => {
+			let renderedString = '';
 			const renderSettings = {
 				throwOnError: false,
 				displayMode: r.display,
 			}
-
-			let renderedString = '';
-
-			const regex = /\\[a-zA-Z]+/;
-			if (regex.test(e)) {
-				renderedString = katex.renderToString(e, renderSettings);
-			} else {
+			if (!hasLatexCommand(e)) {
 				renderedString = wypst.renderToString(e, renderSettings);
+			} else {
+				renderedString = katex.renderToString(e, renderSettings);
 			}
+
 			return parser.parseFromString(renderedString, "text/html").body.firstChild;
 		}
 	}
@@ -44,4 +38,9 @@ export default class Wypst extends Plugin {
 	onunload() {
 		global.MathJax.tex2chtml = this._tex2chtml;
 	}
+}
+
+function hasLatexCommand(expr: string) {
+	const regex = /\\\S/;
+	return regex.test(expr);
 }
